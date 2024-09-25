5122bceed86f9f660c33807f57741850c4f77221
Avadhoot-Kulkarni <kilbil.4629.avadhootixc@gmail.com>
2024-09-25 11:46:45 +0000

    productcontrollers

--- pharmbackend/controllers/productController.js
+++ pharmbackend/controllers/productController.js
@@ -24,28 +24,3 @@ exports.createProduct = async (req, res) => {
   }
 };
 
-import Product from '../models/Product';
-exports.getAllProducts = async (req, res) => {
-  try {
-    const products = await Product.find();
-    res.json(products);
-  } catch (error) {
-    res.status(500).json({ message: 'Server error' });
-  }
-};
-exports.createProduct = async (req, res) => {
-  const { name, description, price, stock, image } = req.body;
-  try {
-    const product = new Product({
-      name,
-      description,
-      price,
-      stock,
-      image,
-    });
-    await product.save();
-    res.status(201).json(product);
-  } catch (error) {
-    res.status(500).json({ message: 'Server error' });
-  }
-};
