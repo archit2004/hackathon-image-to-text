3a1ed96d75fd52d4dbd029c594c4507a2076e935
AdityaBhagat05 <bhagataditya2506@gmail.com>
2024-09-25 12:53:11 +0530

    Created project structure

--- pharmbackend/server.js
+++ pharmbackend/server.js
@@ -2,11 +2,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-
+//Import Routes
+import authRoutes from './routes/authRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+import productRoutes from './routes/productRoutes.js';
 //Initializing express
 const app=express();
 const PORT=3000;
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
 });
-
