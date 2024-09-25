fdc4da2a682e1effef3a0f0afaf7317ab4d8933e
Avadhoot-Kulkarni <kilbil.4629.avadhootixc@gmail.com>
2024-09-25 21:13:23 +0000

    Update userRoutes.js

--- pharmbackend/routes/userRoutes.js
+++ pharmbackend/routes/userRoutes.js
@@ -1,17 +1,16 @@
-
 import express from 'express';
 import User from '../models/User.js';
-import bcrypt from 'bcryptjs'; 
+import bcrypt from 'bcryptjs';
 
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
-    const { username, password, email } = req.body;
-    
+    const { username, password, email, phoneNumber } = req.body; 
+
     try {
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        const newUser = new User({ username, password: hashedPassword, email });
+        const newUser = new User({ username, password: hashedPassword, email, phoneNumber }); 
         await newUser.save();
 
         res.status(201).json({ message: "User registered successfully" });
@@ -35,7 +34,7 @@ router.post('/login', async (req, res) => {
             return res.status(400).json({ message: "Invalid credentials" });
         }
 
-        res.status(200).json({ message: "Login successful", userId: user._id });
+        res.status(200).json({ message: "Login successful", userId: user._id, phoneNumber: user.phoneNumber }); 
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
